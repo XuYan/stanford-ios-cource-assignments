0917905ad832b1e4995c02d956e9b6585c16d496
XuYan <xuwithwill@gmail.com>
2020-02-25 10:43:26 -0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Xu Yan on 2/25/20.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
