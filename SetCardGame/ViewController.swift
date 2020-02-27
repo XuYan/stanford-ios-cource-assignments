3b1a478ef3008c2e51edbd210788b0ee61c824ff
XuYan <xuwithwill@gmail.com>
2020-02-26 19:06:23 -0800

    Initial Commit

---
+++ SetCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetCardGame
+//
+//  Created by Xu Yan on 2/26/20.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
